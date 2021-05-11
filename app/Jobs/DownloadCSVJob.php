7d57f281579440b7ef3ab19a6b940f52badc32ff
Giuliano Vallejos Santajuliana <63474125+YuloVS@users.noreply.github.com>
2021-05-11 20:21:47 -0300

    Added Job to download CSV from datos.gob.ar

---
+++ app/Jobs/DownloadCSVJob.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+
+class DownloadCSVJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private string $downloadLink;
+    private string $filesDirectory;
+    private string $fileName;
+
+    public function __construct(string $downloadLink)
+    {
+        $this->downloadLink = $downloadLink;
+        $this->filesDirectory = storage_path("Vaccines");
+        $this->fileName = "{$this->filesDirectory}\Locations.csv";
+    }
+
+    public function handle()
+    {
+        if(!file_exists($this->filesDirectory))
+        {
+            File::makeDirectory($this->filesDirectory, 0777, true);
+            $file = Http::get('https://sisa.msal.gov.ar/datos/descargas/covid-19/files/Covid19VacunasAgrupadas.csv.zip')->body();
+            Storage::put($this->fileName, $file);
+        }
+    }
+}
