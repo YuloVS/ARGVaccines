c0921ede8317b38cc403971cbdb6396257298630
Giuliano Vallejos Santajuliana <63474125+YuloVS@users.noreply.github.com>
2021-05-12 12:18:52 -0300

    Add methods to sum first and second doses

--- app/Models/QtyByLocation.php
+++ app/Models/QtyByLocation.php
@@ -10,4 +10,21 @@ class QtyByLocation extends Model
     protected $fillable = [
         "province", "vaccine", "first_dose", "second_dose"
     ];
+
+    public static function totalDoses($dose = null)
+    {
+        return $dose ?
+            QtyByLocation::groupBy("province")->selectRaw("sum({$dose}) as total_{$dose}, province")->get() :
+            QtyByLocation::groupBy("province")->selectRaw("sum(first_dose) as total_first_dose, sum(second_dose) as total_second_dose, province")->get();
+    }
+
+    public static function totalFirstDoses()
+    {
+        return self::totalDoses("first_dose");
+    }
+
+    public static function totalSecondDoses()
+    {
+        return self::totalDoses("second_dose");
+    }
 }
