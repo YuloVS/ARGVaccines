c2481d3caafa9ef24ddd9d4b86087c1a4f598890
Giuliano Vallejos Santajuliana <63474125+YuloVS@users.noreply.github.com>
2021-05-12 11:43:12 -0300

    Add sum data to collection

--- app/Http/Resources/QtyByLocationCollection.php
+++ app/Http/Resources/QtyByLocationCollection.php
@@ -4,14 +4,19 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use JetBrains\PhpStorm\ArrayShape;
+use JetBrains\PhpStorm\Pure;
 
 class QtyByLocationCollection extends ResourceCollection
 {
-    #[ArrayShape(["data" => "\Illuminate\Support\Collection", "source" => "string"])] public function toArray($request)
+    #[ArrayShape(["data" => "\Illuminate\Support\Collection", "total_records" => "int", "total_first_dose" => "mixed", "total_second_dose" => "mixed", "source" => "string"])]
+    public function toArray($request)
     : array
     {
         return [
             "data" => $this->collection,
+            "total_records" => $this->collection->count(),
+            "total_first_dose" => $this->collection->sum("first_dose"),
+            "total_second_dose" => $this->collection->sum("second_dose"),
             "source" => "https://sisa.msal.gov.ar/datos/descargas/covid-19/files/Covid19VacunasAgrupadas.csv.zip"
         ];
     }
