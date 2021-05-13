36b2769340cb8dbc0fb59b5a217416ad45fae48e
Giuliano Vallejos Santajuliana <63474125+YuloVS@users.noreply.github.com>
2021-05-13 12:30:53 -0300

    Create 2021_05_13_121351_create_vaccine_registries_table.php

---
+++ database/migrations/2021_05_13_121351_create_vaccine_registries_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVaccineRegistriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vaccine_registries', function (Blueprint $table) {
+            $table->id();
+            $table->char("gender", 1);
+            $table->string("age_range", 15);
+            $table->string("province_of_residence", 20);
+            $table->string("city_of_residence", 120);
+            $table->string("vaccinated_in_the_province", 20);
+            $table->string("vaccinated_in_the_city", 120);
+            $table->date("vaccination_date");
+            $table->string("vaccine", 100);
+            $table->string("vaccination_condition", 10);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vaccine_registries');
+    }
+}
