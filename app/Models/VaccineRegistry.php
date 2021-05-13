91e65f956238a58df0767119488f507995dd206d
Giuliano Vallejos Santajuliana <63474125+YuloVS@users.noreply.github.com>
2021-05-13 12:30:49 -0300

    Create VaccineRegistry.php

---
+++ app/Models/VaccineRegistry.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VaccineRegistry extends Model
+{
+    protected $fillable = [
+        "gender", "age_range", "province_of_residence", "city_of_residence", "vaccinated_in_the_province",
+        "vaccinated_in_the_city", "vaccination_date", "vaccine", "vaccination_condition"
+    ];
+}
