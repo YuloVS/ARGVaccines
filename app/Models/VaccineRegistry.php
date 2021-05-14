c169390ca2b1fbdd8dac2e68aadd5df38d7fa2f3
Giuliano Vallejos Santajuliana <63474125+YuloVS@users.noreply.github.com>
2021-05-14 12:13:21 -0300

    Add scopes

--- app/Models/VaccineRegistry.php
+++ app/Models/VaccineRegistry.php
@@ -12,8 +12,48 @@ class VaccineRegistry extends Model
         "vaccinated_in_the_city", "vaccination_date", "vaccine", "vaccination_condition"
     ];
 
+    public function scopeGender($query, $type)
+    {
+        return $query->where('gender', $type);
+    }
+
+    public function scopeAgeRange($query, $type)
+    {
+        return $query->where('age_range', $type);
+    }
+
     public function scopeProvinceOfResidence($query, $type)
     {
         return $query->where('province_of_residence', $type);
+    }
+
+    public function scopeCityOfResidence($query, $type)
+    {
+        return $query->where('city_of_residence', $type);
+    }
+
+    public function scopeVaccinatedInProvince($query, $type)
+    {
+        return $query->where('vaccinated_in_the_province', $type);
+    }
+
+    public function scopeVaccinatedInCity($query, $type)
+    {
+        return $query->where('vaccinated_in_the_city', $type);
+    }
+
+    public function scopeVaccinationDate($query, $type)
+    {
+        return $query->where('vaccination_date', $type);
+    }
+
+    public function scopeVaccine($query, $type)
+    {
+        return $query->where('vaccine', $type);
+    }
+
+    public function scopeVaccinationCondition($query, $type)
+    {
+        return $query->where('vaccination_condition', $type);
     }
 }
