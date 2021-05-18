951e5b6898fb94606de2e1b1a3f383f88585b5f9
Giuliano Vallejos Santajuliana <63474125+YuloVS@users.noreply.github.com>
2021-05-18 10:23:07 -0300

    Add AggregatedByVaccinationProvinceAndVaccinationCondition model, table and job

---
+++ app/Jobs/InsertAggregatedByVaccinationProvinceAndVaccinationCondition.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\AggregatedByVaccinationProvinceAndVaccinationCondition;
+use App\Models\VaccineRegistry;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+
+class InsertAggregatedByVaccinationProvinceAndVaccinationCondition implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle()
+    {
+        AggregatedByVaccinationProvinceAndVaccinationCondition::truncate();
+        $records = VaccineRegistry::select("vaccinated_in_the_province", "vaccination_condition", DB::raw('count(*) as quantity'))
+            ->groupBy("vaccinated_in_the_province", "vaccination_condition")
+            ->get()->toArray();
+        AggregatedByVaccinationProvinceAndVaccinationCondition::insert($records);
+    }
+}
---
+++ database/migrations/2021_05_18_101825_create_aggregated_by_vaccination_province_and_vaccination_conditions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAggregatedByVaccinationProvinceAndVaccinationConditionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aggregated_by_vaccination_province_and_vaccination_conditions', function (Blueprint $table) {
+            $table->id();
+            $table->string("vaccinated_in_the_province", 20);
+            $table->string("vaccination_condition", 20);
+            $table->unsignedInteger("quantity");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aggregated_by_vaccination_province_and_vaccination_conditions');
+    }
+}
