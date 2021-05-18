e03e9966656430d32ca5c71630293767dc82aa03
Giuliano Vallejos Santajuliana <63474125+YuloVS@users.noreply.github.com>
2021-05-18 09:22:01 -0300

    Add AggregatedByResidenceProvinceAndAgeRange model, table and job

---
+++ app/Jobs/InsertAggregatedByResidenceProvinceAndAgeRangeJob.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\AggregatedByResidenceProvinceAndAgeRange;
+use App\Models\VaccineRegistry;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+
+class InsertAggregatedByResidenceProvinceAndAgeRangeJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle()
+    {
+        AggregatedByResidenceProvinceAndAgeRange::truncate();
+        $records = VaccineRegistry::select("province_of_residence", "age_range", DB::raw('count(*) as quantity'))
+            ->groupBy("vaccinated_in_the_province", "age_range")
+            ->get()->toArray();
+        AggregatedByResidenceProvinceAndAgeRange::insert($records);
+    }
+}
