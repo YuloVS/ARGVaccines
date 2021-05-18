d7ee604b92ae58995d0b68f356f5c86a9042d9da
Giuliano Vallejos Santajuliana <63474125+YuloVS@users.noreply.github.com>
2021-05-18 10:16:56 -0300

    Add AggregatedByResidenceProvinceAndVaccinationCondition model, table and job

---
+++ app/Jobs/InsertAggregatedByResidenceProvinceAndVaccinationCondition.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\AggregatedByResidenceProvinceAndVaccinationCondition;
+use App\Models\VaccineRegistry;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+
+class InsertAggregatedByResidenceProvinceAndVaccinationCondition implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle()
+    {
+        AggregatedByResidenceProvinceAndVaccinationCondition::trucate();
+        $records = VaccineRegistry::select("province_of_residence", "vaccination_condition", DB::raw('count(*) as quantity'))
+            ->groupBy("vaccinated_in_the_province", "vaccination_condition")
+            ->get()->toArray();
+        AggregatedByResidenceProvinceAndVaccinationCondition::insert($records);
+    }
+}
