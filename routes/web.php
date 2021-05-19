4807d26115b47308268cc5b0019058fd62c97ddc
Giuliano Vallejos Santajuliana <63474125+YuloVS@users.noreply.github.com>
2021-05-18 23:34:56 -0300

    Removed test code

--- routes/web.php
+++ routes/web.php
@@ -24,21 +24,6 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/test', function () {
-    \App\Jobs\DownloadCSVJob::dispatchSync("https://sisa.msal.gov.ar/datos/descargas/covid-19/files/datos_nomivac_covid19.zip", "VaccineRegistry.zip");
-    \App\Jobs\InsertAggregateDataByResidenceProvinceAndGenderJob::dispatch();
-    \App\Jobs\InsertAggregateDataByVaccinationProvinceAndAgeRangeJob::dispatch();
-    \App\Jobs\InsertAggregateDataByVaccinationProvinceAndGenderJob::dispatch();
-    \App\Jobs\InsertAggregatedByResidenceProvinceAndAgeRangeJob::dispatch();
-    \App\Jobs\InsertAggregatedByResidenceProvinceAndVaccinationCondition::dispatch();
-    \App\Jobs\InsertAggregatedByResidenceProvinceAndVaccinationDate::dispatch();
-    \App\Jobs\InsertAggregatedByResidenceProvinceAndVaccine::dispatch();
-    \App\Jobs\InsertAggregatedByVaccinationProvinceAndVaccinationCondition::dispatch();
-    \App\Jobs\InsertAggregatedByVaccinationProvinceAndVaccinationDate::dispatch();
-    \App\Jobs\InsertAggregatedByVaccinationProvinceAndVaccine::dispatch();
-    //\App\Jobs\DownloadCSVJob::dispatch("https://sisa.msal.gov.ar/datos/descargas/covid-19/files/Covid19VacunasAgrupadas.csv.zip", "Locations.zip");
-});
-
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
